2de5b30a09f25717591ca11a084a3408b8956410
stanleymsn@gmail.com <stanleymsn@gmail.com@2206fade-0f32-60e9-0828-32c31b987381>
2015-02-25 22:40:45 +0000

    * configuração das anotações para o JPA 2.1

--- trunk/src/main/java/br/ufpi/entity/Produto.java
+++ trunk/src/main/java/br/ufpi/entity/Produto.java
@@ -1,47 +1,68 @@
 package br.ufpi.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+// @Table(name = "produto")
 public class Produto {
-	private Double precoUnitario;
-	private String tipo;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+
+	private Double precoUnitario;
+
+	private String tipo;
+
 	private String descricao;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	/**
 	 * @return the precoUnitario
 	 */
 	public Double getPrecoUnitario() {
 		return precoUnitario;
 	}
+
 	/**
-	 * @param precoUnitario the precoUnitario to set
+	 * @param precoUnitario
+	 *            the precoUnitario to set
 	 */
 	public void setPrecoUnitario(Double precoUnitario) {
 		this.precoUnitario = precoUnitario;
 	}
+
 	/**
 	 * @return the tipo
 	 */
 	public String getTipo() {
 		return tipo;
 	}
+
 	/**
-	 * @param tipo the tipo to set
+	 * @param tipo
+	 *            the tipo to set
 	 */
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
-	
+
 }
