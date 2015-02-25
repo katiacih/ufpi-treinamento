2b0046767f1aab24a930eda234f723f1aaffd0e1
saulodetarsio107 <saulodetarsio107@gmail.com>
2015-02-25 00:09:17 +0000

    inserção dos atributos valor, cliente e id.

--- src/main/java/br/ufpi/entity/Compra.java
+++ src/main/java/br/ufpi/entity/Compra.java
@@ -8,5 +8,26 @@ package br.ufpi.entity;
  *
  */
 public class Compra {
-
+	private int id;
+	private double valor;
+	private Cliente c;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public Cliente getC() {
+		return c;
+	}
+	public void setC(Cliente c) {
+		this.c = c;
+	}
 }
