362596cb0b836cc5285cd62f0a2f50c1835f11e2
gleisondeandradeesilva@gmail.com <gleisondeandradeesilva@gmail.com@2206fade-0f32-60e9-0828-32c31b987381>
2015-02-25 00:07:34 +0000

    Adicionado os atributos da classe e seus getters e setters.

--- trunk/src/main/java/br/ufpi/entity/ItemCompra.java
+++ trunk/src/main/java/br/ufpi/entity/ItemCompra.java
@@ -8,5 +8,26 @@ package br.ufpi.entity;
  *
  */
 public class ItemCompra {
-
+	private Produto produto;
+	private Compra compra;
+	private Long id;
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public Compra getCompra() {
+		return compra;
+	}
+	public void setCompra(Compra compra) {
+		this.compra = compra;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 }
