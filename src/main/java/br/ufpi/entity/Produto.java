b78c48cce07f0896c9ffb33fe0a1c864d8ad7d46
vanderson.moura.vm <vanderson.moura.vm@gmail.com>
2015-02-25 00:08:46 +0000

    atributos inseridos: id e descrição

--- src/main/java/br/ufpi/entity/Produto.java
+++ src/main/java/br/ufpi/entity/Produto.java
@@ -3,6 +3,21 @@ package br.ufpi.entity;
 public class Produto {
 	private Double precoUnitario;
 	private String tipo;
+	private int id;
+	private String descricao;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	/**
 	 * @return the precoUnitario
 	 */
