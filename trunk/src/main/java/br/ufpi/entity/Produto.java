7be308f70f2ca0c4514efe79a8f7b1fb1d9eed3e
RonyerisonBraga@gmail.com <RonyerisonBraga@gmail.com@2206fade-0f32-60e9-0828-32c31b987381>
2015-02-25 00:05:39 +0000

    Criados atributos para produto

--- trunk/src/main/java/br/ufpi/entity/Produto.java
+++ trunk/src/main/java/br/ufpi/entity/Produto.java
@@ -1,5 +1,32 @@
 package br.ufpi.entity;
 
 public class Produto {
-
+	private Double precoUnitario;
+	private String tipo;
+	/**
+	 * @return the precoUnitario
+	 */
+	public Double getPrecoUnitario() {
+		return precoUnitario;
+	}
+	/**
+	 * @param precoUnitario the precoUnitario to set
+	 */
+	public void setPrecoUnitario(Double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 }
