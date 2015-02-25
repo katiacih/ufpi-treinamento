0020b07f90f63ac50b1feab20a31fb3301b4da61
grg.ufpi <grg.ufpi@gmail.com>
2015-02-25 00:10:18 +0000

    Atributos da Classe Cliente

--- src/main/java/br/ufpi/entity/Cliente.java
+++ src/main/java/br/ufpi/entity/Cliente.java
@@ -1,5 +1,73 @@
 package br.ufpi.entity;
 
+import java.util.Date;
+
 public class Cliente {
+	
+	private int id;
+	private int cpf;
+	private String nome;
+	private String email;
+	private String sexo;
+	private Date data_nascimento;
+	private long telefone;
+	private String endereco;
+	private String senha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+	public long getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(long telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 
 }
